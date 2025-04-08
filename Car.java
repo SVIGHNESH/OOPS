255568477a48a521d627b57fe600520e90ac68a1
Vighnesh <vighneshshukla00@gmail.com>
2025-04-08 20:24:52 +0530

    This is the First Commit in the Repository Named OOPS here I just set some getter and setter for the encapsulation

---
+++ Car.java
@@ -0,0 +1,35 @@
+class Car{
+  String color;
+
+  String Company;
+
+  String Model;
+
+  public void setcolor(string color){
+    this.color = color;
+
+  }
+  public void setCompany(String Company){
+    this.Company  = Company;
+  }
+
+  pubic void setModel(String Model){
+    this.Model = Model;
+
+  }
+
+  public String getCompany(){
+    return Company;
+  }
+
+  public String setModel(){
+    return Model;
+  }
+
+
+  public String getcolor(){
+    return color;
+  }
+
+
+}
